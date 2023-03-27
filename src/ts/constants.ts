de2756cc83f6dabdfca9a66fa4453c4015407ead
Manuel Pickl <picklmanuel98@gmail.com>
2023-03-27 17:39:08 +0200

    fixed missing variable debugMode

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,6 +1,6 @@
 const framerate = 5;
 
-const virusWidth = 5;
+const virusWidth = 8;
 const virusHeight = virusWidth;
 
 const minPopulation = 1;
@@ -8,5 +8,6 @@ const overPopulation = 10;
 const spawnProbability = 0.001;
 
 // debug
+const debugMode = true;
 const logCyclus = false;
 const seaClickTest = true;
