e39250bf508eee1bddb545b634b2ff8d2095f37e
berkay <yurdaguel.b@gmail.com>
2023-04-03 17:21:22 +0200

    implemented new sea matrix calculation

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,6 +1,6 @@
-// intervals 
-var simulationInterval: number;
-var hudInterval: number;
+// intervals
+var simulationInterval: any;
+var hudInterval: any;
 
 // matrices
 var virusMatrix: number[][];
@@ -21,8 +21,6 @@ var overPopulation: number = 7;
 var possibleVirusCount: number = 0;
 var infectedCount: number = 0;
 
-
-
 function onSvgLoad() {
   assignHtmlVariables();
   assignHtmlEvents();
@@ -40,26 +38,27 @@ function initializeSimulation() {
   determineSvgSize();
   createMatrices();
   // placeVirusRandomly();
-  
-  if (debugMode) console.log("simulation initialized");
+
+  if (debugMode) console.log('simulation initialized');
 }
 
 function startSimluation() {
   simulationInterval = setInterval(draw, 1000 / maxFramerate);
   hudInterval = setInterval(updateHud, 1000 / maxHudFramerate);
-  
-  (<HTMLElement>document.querySelector(".splash-screen")).style.visibility = "hidden";
+
+  (<HTMLElement>document.querySelector('.splash-screen')).style.visibility =
+    'hidden';
 
   enableVirusPlacement();
 
-  if (debugMode) console.log("simulation started");
+  if (debugMode) console.log('simulation started');
 }
 
-function createMatrices()  {
-  createSeaMatrix();
-  createVirusMatrix()
+function createMatrices() {
+  createVirusMatrix();
+  seaMatrix = createSeaMatrix(worldSvgElement);
 
-  if (debugMode) console.log("game matrices created");
+  if (debugMode) console.log('game matrices created');
 }
 
 function createVirusMatrix() {
@@ -70,7 +69,7 @@ function createVirusMatrix() {
       virusMatrix[column][row] = 0;
     }
   }
-  
+
   virusMatrixNextStep = new Array(virusColumns);
   for (let column = 0; column < virusColumns; column++) {
     virusMatrixNextStep[column] = new Array(virusRows);
@@ -83,18 +82,18 @@ function createVirusMatrix() {
   for (let row = 0; row < virusRows; row++) {
     for (let column = 0; column < virusColumns; column++) {
       // ToDo: try to skip virus tiles that are in the sea
-      var virus = document.createElement("div");
+      var virus = document.createElement('div');
       virus.id = `${column}-${row}`;
-      virus.classList.add("virus");
-      virus.classList.add((Math.random() < 0.5) ? "alpha" : "beta");
+      virus.classList.add('virus');
+      virus.classList.add(Math.random() < 0.5 ? 'alpha' : 'beta');
       let positionX = column * virusWidth;
       if (row % 2 == 1) positionX += virusWidth / 2;
       let positionY = row * virusHeight * 0.75;
       virus.style.left = `${positionX}px`;
       virus.style.top = `${positionY}px`;
-      virus.style.width =`${virusWidth}px`;
+      virus.style.width = `${virusWidth}px`;
       virus.style.height = `${virusHeight}px`;
-      virus.style.opacity = "0";
+      virus.style.opacity = '0';
       // virus.style.visibility = "hidden";
 
       virusMapElement.appendChild(virus);
@@ -104,35 +103,32 @@ function createVirusMatrix() {
 
 function draw() {
   cycleCount++;
-      
-  for ( let row = 0; row < virusRows; row++) {
-    for ( let column = 0; column < virusColumns; column++) {
+
+  for (let row = 0; row < virusRows; row++) {
+    for (let column = 0; column < virusColumns; column++) {
       let currentVirus = document.getElementById(`${column}-${row}`);
 
       if (virusMatrix[column][row] == 1) {
-        currentVirus.style.opacity = "0.5";
-      }
-      else if (virusMatrix[column][row] == 0) {
-        currentVirus.style.opacity = "0";
-      }
-      else console.log("should never happen: ", virusMatrix[column][row]);
+        currentVirus.style.opacity = '0.5';
+      } else if (virusMatrix[column][row] == 0) {
+        currentVirus.style.opacity = '0';
+      } else console.log('should never happen: ', virusMatrix[column][row]);
     }
   }
-  
+
   generate();
 
-  if (logCyclus) console.log("==========");
+  if (logCyclus) console.log('==========');
 }
 
 function placeVirusRandomly() {
-  for ( let row = 0; row < virusRows; row++) {
+  for (let row = 0; row < virusRows; row++) {
     for (let column = 0; column < virusColumns; column++) {
       let randomNumber = 0;
 
       if (pointInSea(column, row)) {
         randomNumber = 0;
-      }
-      else if (Math.random() <= spawnProbability) {
+      } else if (Math.random() <= spawnProbability) {
         randomNumber = 1;
         infectedCount++;
       }
@@ -143,7 +139,7 @@ function placeVirusRandomly() {
     }
   }
 
-  console.log("start point set for virus");
+  console.log('start point set for virus');
 }
 
 function generate() {
@@ -158,26 +154,28 @@ function generate() {
         continue;
       }
 
-      gameOfLife(column, row)   
+      gameOfLife(column, row);
 
       if (virusMatrixNextStep[column][row] == 1) infectedCount++;
       possibleVirusCount++;
     }
   }
 
-  virusMatrix = virusMatrixNextStep.map(function(arr) {
+  virusMatrix = virusMatrixNextStep.map(function (arr) {
     return arr.slice();
   });
 }
 
 function enableVirusPlacement() {
-  document.onmousedown = (e) => { 
+  document.onmousedown = (e) => {
     mouseIsDown = true;
     placeVirus(e);
   };
-  document.onmouseup = () => mouseIsDown = false;
+  document.onmouseup = () => (mouseIsDown = false);
 
-  document.querySelectorAll(".virus").forEach(virus => (<HTMLElement>virus).onmouseover = placeVirus);
+  document
+    .querySelectorAll('.virus')
+    .forEach((virus) => ((<HTMLElement>virus).onmouseover = placeVirus));
 }
 
 function placeVirus(e: MouseEvent) {
@@ -185,30 +183,34 @@ function placeVirus(e: MouseEvent) {
 
   // clicked element has to be a virus tile
   let virus = <HTMLElement>e.target;
-  if (!virus.classList.contains("virus")) return;
+  if (!virus.classList.contains('virus')) return;
 
   // determine column and column of clicked virus tile
-  let column = parseInt(virus.id.split("-")[0]);
-  let row = parseInt(virus.id.split("-")[1]);
-  
+  let column = parseInt(virus.id.split('-')[0]);
+  let row = parseInt(virus.id.split('-')[1]);
+
   // get all relevant virus tiles
   let virusTiles = new Array<[number, number]>();
-  virusTiles.push([column, row]);                     // virus tile on mouse point           
-  getNeighbors(column, row, brushSize - 1)
-    .forEach(neighbor => virusTiles.push(neighbor));  // all neighboring virus tiles
+  virusTiles.push([column, row]); // virus tile on mouse point
+  getNeighbors(column, row, brushSize - 1).forEach((neighbor) =>
+    virusTiles.push(neighbor)
+  ); // all neighboring virus tiles
 
   // enable all virus tiles that are not in the sea
-  virusTiles.forEach(neighbor => {
+  virusTiles.forEach((neighbor) => {
     if (pointInSea(neighbor[0], neighbor[1])) return;
     virusMatrix[neighbor[0]][neighbor[1]] = 1;
-    
+
     // we can already make the tile visible, but it's functionality/spreading only starts on next frame
-    document.getElementById(`${neighbor[0]}-${neighbor[1]}`).style.opacity = "0.5"
+    document.getElementById(`${neighbor[0]}-${neighbor[1]}`).style.opacity =
+      '0.5';
   });
 }
 
 function updateHud() {
   cycleCounterElement.innerText = cycleCount.toString();
   infectedCountElement.innerText = infectedCount.toString();
-  healthyCountElement.innerText = (possibleVirusCount - infectedCount).toString();
+  healthyCountElement.innerText = (
+    possibleVirusCount - infectedCount
+  ).toString();
 }
--- src/ts/svgHelper.ts
+++ src/ts/svgHelper.ts
@@ -1,69 +1,102 @@
-var svgObject: any = document.getElementById("svgObject");
-svgObject?.addEventListener("load", onSvgLoad, false);
+var svgObject: any = document.getElementById('svgObject');
+svgObject?.addEventListener('load', onSvgLoad, false);
 
 function determineSvgSize() {
-    // calculate best fit for svg
-    let width: number = getWidth();
-    let height: number = getHeight();
-  
-    let aspectHeight = Math.floor(width / 2.333);
-    height = height < aspectHeight ? height : aspectHeight;
-    
-    let aspectWidth = Math.floor(height * 2.333);
-    width = width < aspectWidth ? width : aspectWidth;
-  
-    // calculate grid based on svg and virus size
-    virusColumns = Math.floor(((width - virusWidth / 2) / (virusWidth)));
-    virusRows = Math.floor((height - virusHeight * 0.25) / (virusHeight * 0.75));
-    
-    // adjust size to fit grid
-    let calcWidth = (virusColumns + 0.5) * virusWidth;
-    let calcHeight = (virusRows - 1) * virusHeight * 3/4 + virusHeight;
-  
-    // set sizes
-    svgObject.style.width = `${width}px`;
-    svgObject.style.height = `${height}px`;
-    virusMapElement.style.width = `${calcWidth}px`;
-    virusMapElement.style.height = `${calcHeight}px`;
+  // calculate best fit for svg
+  let width: number = getWidth();
+  let height: number = getHeight();
 
-    console.log("svg size determined");
+  let aspectHeight = Math.floor(width / 2.333);
+  height = height < aspectHeight ? height : aspectHeight;
+
+  let aspectWidth = Math.floor(height * 2.333);
+  width = width < aspectWidth ? width : aspectWidth;
+
+  // calculate grid based on svg and virus size
+  virusColumns = Math.floor((width - virusWidth / 2) / virusWidth);
+  virusRows = Math.floor((height - virusHeight * 0.25) / (virusHeight * 0.75));
+
+  // adjust size to fit grid
+  let calcWidth = (virusColumns + 0.5) * virusWidth;
+  let calcHeight = ((virusRows - 1) * virusHeight * 3) / 4 + virusHeight;
+
+  // set sizes
+  svgObject.style.width = `${width}px`;
+  svgObject.style.height = `${height}px`;
+  virusMapElement.style.width = `${calcWidth}px`;
+  virusMapElement.style.height = `${calcHeight}px`;
+
+  console.log('svg size determined');
 }
 
-function createSeaMatrix() {
-  seaMatrix = new Array(virusColumns);
-  for (let column = 0; column < virusColumns; column++) {
-    seaMatrix[column] = new Array(virusRows);
+const isOnLandCache = new Map<string, boolean>();
+
+function preprocessWorldSvg(
+  worldSvgElement: HTMLElement,
+  columns: number,
+  rows: number
+): Uint8Array {
+  const lookupTable = new Uint8Array(columns * rows);
+
+  for (let row = 0; row < rows; row++) {
+    for (let column = 0; column < columns; column++) {
+      let positionX = column * virusWidth;
+      if (row % 2 === 1) positionX += virusWidth / 2;
+      let positionY = row * virusHeight * 0.75;
+
+      const onLand =
+        worldSvgElement.ownerDocument.elementFromPoint(
+          positionX + virusWidth / 2,
+          positionY + virusHeight / 2
+        ) !== worldSvgElement;
+      lookupTable[column + row * columns] = onLand ? 1 : 0;
+    }
   }
 
+  return lookupTable;
+}
+
+function isOnLand(
+  positionX: number,
+  positionY: number,
+  worldSvgElement: HTMLElement
+): boolean {
+  const cacheKey = `${positionX},${positionY}`;
+  if (isOnLandCache.has(cacheKey)) {
+    return isOnLandCache.get(cacheKey)!;
+  }
+
+  const onLand =
+    worldSvgElement.ownerDocument.elementFromPoint(
+      positionX + virusWidth / 2,
+      positionY + virusHeight / 2
+    ) !== worldSvgElement;
+
+  isOnLandCache.set(cacheKey, onLand);
+  return onLand;
+}
+
+function createSeaMatrix(worldSvgElement: any): number[][] {
+  const lookupTable = preprocessWorldSvg(
+    worldSvgElement,
+    virusColumns,
+    virusRows
+  );
+  seaMatrix = Array.from({ length: virusColumns }, () => new Array(virusRows));
 
   for (let row = 0; row < virusRows; row++) {
     for (let column = 0; column < virusColumns; column++) {
-      let positionX = column * virusWidth;
-      if (row % 2 == 1) positionX += virusWidth / 2;
-      let positionY = row * virusHeight * 0.75;
-
-      let pointOnLand = (worldSvgElement.ownerDocument.elementFromPoint(positionX + virusWidth / 2, positionY + virusHeight / 2) != worldSvgElement)
-        // sides
-        || (worldSvgElement.ownerDocument.elementFromPoint(positionX + virusWidth / 2, positionY) != worldSvgElement) // top
-        || (worldSvgElement.ownerDocument.elementFromPoint(positionX + virusWidth, positionY + virusHeight / 2) != worldSvgElement) // right
-        || (worldSvgElement.ownerDocument.elementFromPoint(positionX + virusWidth / 2, positionY + virusHeight) != worldSvgElement) // bottom
-        || (worldSvgElement.ownerDocument.elementFromPoint(positionX, positionY + virusHeight / 2) != worldSvgElement) // left
-        // edges
-        // || (worldSvg.ownerDocument.elementFromPoint(positionX, positionY) != worldSvg) // top left
-        // || (worldSvg.ownerDocument.elementFromPoint(positionX + virusWidth, positionY) != worldSvg) // top right
-        // || (worldSvg.ownerDocument.elementFromPoint(positionX + virusWidth, positionY + virusHeight) != worldSvg) // bottom right
-        // || (worldSvg.ownerDocument.elementFromPoint(positionX, positionY + virusHeight) != worldSvg) // bottom left
-
-      seaMatrix[column][row] = pointOnLand ? 1 : 0;
+      seaMatrix[column][row] = lookupTable[column + row * virusColumns];
     }
   }
+
+  return seaMatrix;
 }
-  
+
 function getCountry(x, y) {
-    // let path = worldSvg.ownerDocument.elementFromPoint(x, y);
-    // // console.log(path);
-  
-    // // let pointInGermany = worldSvg.ownerDocument.elementFromPoint(e.x, e.y) == de;
-    // let country = path.getAttribute("name") ?? path.className.baseVal ?? path.id;
-    // console.log(country == "" ? "Sea" : country);
+  // let path = worldSvg.ownerDocument.elementFromPoint(x, y);
+  // // console.log(path);
+  // // let pointInGermany = worldSvg.ownerDocument.elementFromPoint(e.x, e.y) == de;
+  // let country = path.getAttribute("name") ?? path.className.baseVal ?? path.id;
+  // console.log(country == "" ? "Sea" : country);
 }
